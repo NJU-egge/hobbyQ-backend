f49f68a8ab268d97283689b5b7a8d25a90a6a46f
sun <221900209@smail.nju.edu.cn>
2024-08-01 01:32:08 +0800

    修改端口号至22209

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,7 +4,7 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1722309381916_9912',
   koa: {
-    port: 7001,
+    port: 22209,
   },
   cors: {
     origin: '*',
